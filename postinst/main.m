babc5a48cad95622388458daf6912669876fe13a
woodfairy <d.schmit@m3connect.de>
2020-08-23 14:05:20 +0200

    spotify (WIP)

--- postinst/main.m
+++ postinst/main.m
@@ -2,10 +2,10 @@
 #import <objc/runtime.h>
 #import "../Tweak/MusicLyricsBackgroundView.h"
 
-
-
 int main(int argc, char *argv[], char *envp[]) {
 	@autoreleasepool {
+		UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"Cyan postinst" message:@"Hello World" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
+		[alertView show];
 		NSString *frameworkPath = [objc_getClass("LSApplicationProxy") applicationProxyForIdentifier:@"com.apple.Music"].bundleURL.resourceSpecifier;
 		NSString *sourcePath;
 		NSString *destPath = @"/Library/Frameworks/CyanFrameworks/";
@@ -19,12 +19,9 @@ int main(int argc, char *argv[], char *envp[]) {
 		BOOL isDirectory;
 		sourcePath = [frameworkPath stringByAppendingPathComponent:@"Frameworks/MusicApplication.framework/"];
 		NSLog(@"path in postinst %@", sourcePath);
-		for (NSString *currentFile in sourceFiles)
-		{
-			if ([fm fileExistsAtPath:currentFile isDirectory:&isDirectory] && !isDirectory)
-			{
-				if (![fm copyItemAtPath:[sourcePath stringByAppendingPathComponent:currentFile] toPath:[destPath stringByAppendingPathComponent:currentFile] error:&copyError])
-				{
+		for (NSString *currentFile in sourceFiles) {
+			if ([fm fileExistsAtPath:currentFile isDirectory:&isDirectory] && !isDirectory) {
+				if (![fm copyItemAtPath:[sourcePath stringByAppendingPathComponent:currentFile] toPath:[destPath stringByAppendingPathComponent:currentFile] error:&copyError]) {
 					UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"Cyan postinst" message:[copyError description] delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles:nil];
 					[alertView show];
 				}
